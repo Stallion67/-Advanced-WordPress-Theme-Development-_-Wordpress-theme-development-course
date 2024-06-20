484badefadbf843fcbe2100cbda3d0f99d36e165
Ben Okello Mwaka <mwakaben@gmail.com>
2024-06-20 04:08:55 +0300

    addding custom styles

--- app/public/wp-content/themes/Atila/functions.php
+++ app/public/wp-content/themes/Atila/functions.php
@@ -4,3 +4,21 @@
  * 
  * @package Atila
  */
+
+//  echo '<pre>';
+//  print_r(filemtime(get_template_directory(). '/style.css'));
+//  wp_die();
+
+ //adding a style sheet
+function atila_enqueue_scripts(){
+    //loadidng custom style 
+   // wp_enqueue_style('stylesheet', get_template_directory_uri(). '/style.css');
+    
+   wp_enqueue_style( 'stylesheet',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
+
+}
+ 
+add_action('wp_enqueue_scripts', 'atila_enqueue_scripts');
+
+//endidng tags
+?>
