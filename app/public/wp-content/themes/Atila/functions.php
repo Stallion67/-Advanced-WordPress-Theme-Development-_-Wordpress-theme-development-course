823b8c952d268b0db4cf5162f931288c6e5044ca
Ben Okello Mwaka <mwakaben@gmail.com>
2024-06-20 04:23:50 +0300

    registering then enqueing the styles and scripts

--- app/public/wp-content/themes/Atila/functions.php
+++ app/public/wp-content/themes/Atila/functions.php
@@ -11,11 +11,16 @@
 
  //adding a style sheet
 function atila_enqueue_scripts(){
-    //loadidng custom style 
-   // wp_enqueue_style('stylesheet', get_template_directory_uri(). '/style.css');
     
-   wp_enqueue_style( 'style-css',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
-   wp_enqueue_script('main-js',get_template_directory_uri(). '/assets/main.js', [],filemtime( get_template_directory() . '/assets/main.js'), true );
+  //registering the styles and scripts
+   wp_register_style( 'style-css',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
+   wp_register_script('main-js',get_template_directory_uri(). '/assets/main.js', [],filemtime( get_template_directory() . '/assets/main.js'), true );
+
+   //enqueindg the style and scripts
+   wp_enqueue_style( 'style-css');
+   wp_enqueue_script('main-js');
+
+
 
 }
  
