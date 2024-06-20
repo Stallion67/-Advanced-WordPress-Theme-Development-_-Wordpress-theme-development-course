8a9608b846ff173db582438cb6af7f60d2bf483a
Ben Okello Mwaka <mwakaben@gmail.com>
2024-06-20 04:19:00 +0300

    madding custom Js- javascript

--- app/public/wp-content/themes/Atila/functions.php
+++ app/public/wp-content/themes/Atila/functions.php
@@ -14,7 +14,8 @@ function atila_enqueue_scripts(){
     //loadidng custom style 
    // wp_enqueue_style('stylesheet', get_template_directory_uri(). '/style.css');
     
-   wp_enqueue_style( 'stylesheet',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
+   wp_enqueue_style( 'style-css',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
+   wp_enqueue_script('main-js',get_template_directory_uri(). '/assets/main.js', [],filemtime( get_template_directory() . '/assets/main.js'), true );
 
 }
  
